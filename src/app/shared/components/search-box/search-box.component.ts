1d2010b6654ec30e41ddcd560340179e8792282f
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-11-27 16:33:52 +0100

    Funcionamiento SearchBox

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -9,4 +9,13 @@ import { Component, Input } from '@angular/core';
 export class SearchBoxComponent {
   @Input()
   public placeholder: string = '';
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+
+  public emitValue(text: string):void {
+    if(this.placeholder === '') return;
+
+    this.onValue.emit(text);
+  }
 }
