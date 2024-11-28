d139f66122c72c088b94b3d3f2652117522cb739
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-11-28 09:24:09 +0100

    Countries - Mostrar capital por pantalla

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-by-capital-page',
@@ -7,8 +9,16 @@ import { Component } from '@angular/core';
 })
 
 export class ByCapitalPageComponent {
+  constructor(private countriesService: CountriesService) {}
+
+  public countries: Country[] = [];
+
   searchByCapital(term: string): void {
-    console.log('Desde ByCapitalPage');
-    console.log({term});
+    if(term === null) return;
+
+    this.countriesService.searchCapital(term)
+      .subscribe(countries => {
+        this.countries = countries;
+      })
   }
 }
