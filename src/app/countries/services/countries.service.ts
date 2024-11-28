8c066665d5ea7bb3d76ec7a351bb19cd4abacad9
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-11-28 11:18:40 +0100

    CatchError - Manejo de errores

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -13,6 +13,10 @@ export class CountriesService {
   searchCapital(term: string): Observable<Country[]> {
     const url: string = `${this.apiUrl}/capital/${term}`;
 
-    return this.httpClient.get<Country[]>(url);
+    return this.httpClient.get<Country[]>(url).pipe(
+       tap(countries => console.log('Tap 1', countries)),
+       map(countries => []),
+       tap(countries => console.log('Tap 2', countries))
+    );
   }
 }
