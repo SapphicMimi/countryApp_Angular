72f2aaf60875e7c9f65caefb8c82ac0f4cf4ded3
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-11-28 11:35:14 +0100

    CatchError - Manejo de errores v2

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -14,9 +14,19 @@ export class CountriesService {
     const url: string = `${this.apiUrl}/capital/${term}`;
 
     return this.httpClient.get<Country[]>(url).pipe(
+      /*
        tap(countries => console.log('Tap 1', countries)),
        map(countries => []),
        tap(countries => console.log('Tap 2', countries))
+
+      catchError(error => {
+        console.log(error);
+
+        return of([])
+
+      })
+      */
+     catchError(() => ([]))
     );
   }
 }
