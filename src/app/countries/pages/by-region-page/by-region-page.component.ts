ea1b5a775928273e3614146d36fcad841bf3f481
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-02 15:35:11 +0100

    Optimización - Buscar por Región

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'America' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -13,11 +15,15 @@ export class ByRegionPageComponent {
 
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
-  searchByRegion(term: string): void {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
+
     this.isLoading = true;
 
-    this.countriesService.searchRegion(term)
+    this.countriesService.searchRegion(region)
       .subscribe(countries => {
         this.countries = countries;
         this.isLoading = false;
